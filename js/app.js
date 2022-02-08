9373a24661d22c7b121738f5f745c842b0c5e98e
Monse Vásquez <47286468+MonseV@users.noreply.github.com>
2022-02-08 16:26:56 -0600

    reproducción de canción
    seleccionando una por una

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
   getSongs();
 });
 
-var audio;
+var audio = document.getElementById('player');
 var music;
 
 function getSongs(){
@@ -13,6 +13,13 @@ function getSongs(){
   });
 }
 
+function playSong(id){
+  console.log(id);
+  $('#img-album').attr('src', music.songs[id].image);
+  $('#player').attr('src', music.songs[id].song);
+  audio.play();
+}
+
 function genList(music){
   $.each(music.songs, function(i, song){
     $('#playlist').append('<li class="list-group-item" id="'+i+'">'+song.name+'</li>')
@@ -20,6 +27,7 @@ function genList(music){
 
   $('#playlist li').click(function(){
     var selectedsong = $(this).attr('id');
-    console.log(selectedsong);
+    /* console.log(selectedsong); */
+    playSong(selectedsong);
   });
 }
