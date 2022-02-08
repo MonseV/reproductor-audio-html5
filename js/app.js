84f1579c5389e8f9bed341d37f60111e58a48dd3
Monse Vásquez <47286468+MonseV@users.noreply.github.com>
2022-02-08 15:51:36 -0600

    Creación de Lista de Canciones

--- js/app.js
+++ js/app.js
@@ -8,6 +8,18 @@ var music;
 function getSongs(){
   $.getJSON("js/app.json", function(mjson){
     music = mjson;
-    console.log(music);
+    // console.log(music);
+    genList(music);
+  });
+}
+
+function genList(music){
+  $.each(music.songs, function(i, song){
+    $('#playlist').append('<li class="list-group-item" id="'+i+'">'+song.name+'</li>')
+  });
+
+  $('#playlist li').click(function(){
+    var selectedsong = $(this).attr('id');
+    console.log(selectedsong);
   });
 }
